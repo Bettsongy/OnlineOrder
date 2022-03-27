16cb4c87af23f19eb81d049a02eab852778bc7a2
Bettsongy <704443188@qq.com>
2022-03-27 16:58:41 -0600

    First commit

---
+++ src/main/java/com/laioffer/onlineorder/HelloKevin.java
@@ -0,0 +1,31 @@
+package com.laioffer.onlineorder;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// the value is the actual trigger
+@WebServlet(name = "helloKevin", value = "/hello-Kevin")
+public class HelloKevin extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello Kevin!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
